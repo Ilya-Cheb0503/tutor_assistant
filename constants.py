b9ab62f345bb1fcb127323d043ba445a18c60924
Ilya-Cheb0503 <cheb.ilya05@yandex.ru>
2024-12-15 21:51:18 +0300

    students.db was deleted from rep

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 TEACHER_ID = 5086356786
 CALENDAR_ID = 'rinigudini@gmail.com'
-CREDENTIALS_FILE = '/var/www/u2936676/data/tutor/tutor_assistant/chemical-tutor-ec8a32045bb2.json'
+CREDENTIALS_FILE = r'C:\dev_py\chemical_tutor\calendar_project\project\chemical-tutor-ec8a32045bb2.json'
