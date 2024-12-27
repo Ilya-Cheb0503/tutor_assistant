7f04f66628fc690de7c249d5125b06bc15b9390c
Ilya-Cheb0503 <cheb.ilya05@yandex.ru>
2024-12-27 20:42:56 +0300

    update constants file

--- constants/constants.py
+++ constants/constants.py
@@ -6,5 +6,6 @@ special_users = [
     ]
 
 CALENDAR_ID = 'rinigudini@gmail.com'
-CREDENTIALS_FILE = r'\json_docs\chemical-tutor-ec8a32045bb2.json'
-DOWNLOAD_PATH = r'\messages_cash'
+CREDENTIALS_FILE = r'/json_docs/chemical-tutor-ec8a32045bb2.json'
+DOWNLOAD_PATH = r'/messages_cash'
+NOTIFICATIONS_FILE = '/notifications.json'
