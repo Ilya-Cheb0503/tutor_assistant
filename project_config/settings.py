04384447b6c813b68c6bfc12c18156d39211db34
Ilya-Cheb0503 <cheb.ilya05@yandex.ru>
2025-01-03 17:47:11 +0300

    update logging setting to global project settings

--- project_config/settings.py
+++ project_config/settings.py
@@ -1,12 +1,19 @@
 import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+log_directory = 'logs'
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
+
+log_file_path = os.path.join(log_directory, 'common_logs.log')
+handler = RotatingFileHandler(log_file_path, maxBytes=5*1024*1024, backupCount=10, encoding='utf-8')
+formatter = logging.Formatter('%(levelname)s\n%(asctime)s\n%(message)s\n'
+                              '%(name)s, %(filename)s, '
+                              '%(funcName)s, %(lineno)s\n')
+handler.setFormatter(formatter)
 
 logging.basicConfig(
     level=logging.INFO,
-    filename='common_logs.log',
-
-    format=('%(levelname)s\n%(asctime)s\n%(message)s\n'
-            '%(name)s, %(filename)s, '
-            '%(funcName)s, %(lineno)s\n'),
-    filemode='w',
-    encoding='utf-8'
+    handlers=[handler],
 )
